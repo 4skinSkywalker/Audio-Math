9baab0b1faed253dbf90f717f52fb9e8eb040fba
Fred's GitHub <stopchemtrailsfred@gmail.com>
2016-12-12 22:13:40 +0100

    Fix

--- AudioMath.js
+++ AudioMath.js
@@ -163,7 +163,7 @@ AudioMath.prototype.generateArray = function() {
 			var first 	= this.randomNumber(this.n1min, this.n1MAX),
 				second 	= this.randomNumber(this.n2min, this.n2MAX);
 				
-			procedure.push(first + " + " + second + " = " + first + second);
+			procedure.push(first + " + " + second + " = " + eval(first + second));
 			this.series.push(procedure);
 		}
 	}
