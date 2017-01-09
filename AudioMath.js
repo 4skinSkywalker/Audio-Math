bdcda9366b67310d62b3cdadfdd5c1c15b7ede09
Fred's GitHub <stopchemtrailsfred@gmail.com>
2017-01-09 17:03:20 +0100

    Fix

--- AudioMath.js
+++ AudioMath.js
@@ -14,9 +14,9 @@ engine.strings		= [];
 engine.calculations = { "type":"range", "target":"calculations", "text":"Calculations:", "value":50, "min":10, "step":10, "MAX":500};
 engine.numbers 		= { "type":"range", "target":"numbers", "text":"Numbers:", "value":5, "min":2, "step":1, "MAX":10};
 engine.digits 		= { "type":"range", "target":"digits", "text":"Digits:", "value":1, "min":1, "step":1, "MAX":2};
-engine.delay 		= { "type":"range", "target":"abacus-delay", "text":"Abacus Delay:", "value":250, "min":0, "step":50, "MAX":500, "char":"%", "change":function(x) {return x/5}};
-engine.rate 		= { "type":"range", "target":"speech-rate", "text":"Speech Rate:", "value":1.0, "min":0.5, "step":0.1, "MAX":2, "char":"%", "change":function(x) {return Math.round(x*100)}};
-engine.voice		= { "type":"selector", "target":"speech-voices", "text":"Speech Voice:", "value":0, "selection": []};
+engine.delay 		= { "type":"range", "target":"abacus-delay", "text":"elay:", "value":250, "min":0, "step":50, "MAX":500, "char":"ms"};
+engine.rate 		= { "type":"range", "target":"speech-rate", "text":"Rate:", "value":1.0, "min":0.5, "step":0.1, "MAX":2, "char":"%", "change":function(x) {return Math.round(x*100)}};
+engine.voice		= { "type":"selector", "target":"speech-voices", "text":"Voice:", "value":0, "selection": []};
 engine.operation	= { "type":"selector", "target":"operation", "text":"Operation:", "value":"+/-",
 						"selection": [
 							"+/-",
